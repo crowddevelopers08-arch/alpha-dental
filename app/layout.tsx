57e006d32881404f9cd1a13328c133e78702c288
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-07-23 11:22:28 +0530

    create a new lp for alpha dental clinicssddssdddd

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,23 +7,23 @@ export const metadata: Metadata = {
     icons: {
     icon: [
       {
-        url: "/image.png",
+        url: "/logos.png",
         sizes: "any",
       },
       {
-        url: "/image.png",
+        url: "/logos.png",
         sizes: "16x16",
         type: "image/png",
       },
       {
-        url: "/image.png",
+        url: "/logos.png",
         sizes: "32x32",
         type: "image/png",
       },
     ],
     apple: [
       {
-        url: "/image.png",
+        url: "/logos.png",
         sizes: "180x180",
         type: "image/png",
       },
@@ -31,13 +31,13 @@ export const metadata: Metadata = {
     other: [
       {
         rel: "icon",
-        url: "/image.png",
+        url: "/logos.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
         rel: "icon",
-        url: "/image.png",
+        url: "/logos.png",
         sizes: "512x512",
         type: "image/png",
       },
