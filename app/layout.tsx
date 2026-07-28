62b6195409c5d026f88ae29631d29c7e595bc0de
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-07-28 17:32:37 +0530

    create a new lp for alpha dental clinics attached meta pic=xel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Dentist & Medical Odontologist",
   description: "Modern dental and medical WordPress theme showcase",
-    icons: {
+  icons: {
     icon: [
       {
         url: "/logos.png",
@@ -52,7 +53,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col font-sans">{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T468K3PK');
+          `}
+        </Script>
+      </head>
+
+      <body className="min-h-full flex flex-col font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T468K3PK"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
