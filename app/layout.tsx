18d21608cce8521e27f2d23d263aad2f89a907cc
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-08-08 13:22:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en">
       <head>
         {/* Google Tag Manager */}
         <Script
@@ -65,7 +65,8 @@ export default function RootLayout({
                 w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});
                 var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
                 j.async=true;
                 j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
                 f.parentNode.insertBefore(j,f);
@@ -92,9 +93,47 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {
+                if(f.fbq)return;
+                n=f.fbq=function(){
+                  n.callMethod ?
+                  n.callMethod.apply(n,arguments) :
+                  n.queue.push(arguments)
+                };
+                if(!f._fbq)f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(
+                window,
+                document,
+                'script',
+                'https://connect.facebook.net/en_US/fbevents.js'
+              );
+
+              fbq('init', '2220089875502538');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
 
       <body className="min-h-full flex flex-col font-sans">
+
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
@@ -105,6 +144,17 @@ export default function RootLayout({
               display: "none",
               visibility: "hidden",
             }}
+          />
+        </noscript>
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2220089875502538&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
 
